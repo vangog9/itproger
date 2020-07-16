6f6a7239490cc651beca0b0af48eb73e2ca8e4ea
vangog9 <vangog9@mail.ru>
2020-07-16 16:48:26 +0300

    lesson3

---
+++ resources/views/about.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('title-block')
+    Эбаут
+@endsection
+
+@section('content')
+
+<h1> About </h1>
+
+    <p>lsdhfgkljsgkljdshn;fljgjljsdnbfvkljngskljngk gsmlkfgnldnfsg sgfkn;lsngf;ljsndfg;l ;ksfg;lnk</p>
+
+@endsection
