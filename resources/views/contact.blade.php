d042a2689f0fb6a33ee468dc61bdcfc9226e1558
Laravel-tester <vangog9@mail.ru>
2020-08-13 14:31:09 +0300

    lesson3-1

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -8,7 +8,9 @@
 
     <h1> Contact </h1>
 
-    <form action="/contact/submit" method="post">
+    <form action="{{route('contact-form')}}" method="post">
+        @csrf
+
         <div class="form-group">
             <label for="name">Введите имя</label>
             <input type="text" name="name" placeholder="Введите имя" id="name" class="form-control">
@@ -21,8 +23,16 @@
 
         <div class="form-group">
             <label for="subject">Тема сообщения</label>
-            <input type="text" name="subject" placeholder="Тема сообщения" id="subject" class="form-control">
+            <input type="text" name="subject" placeholder="222 сообщения" id="subject" class="form-control">
         </div>
+
+        <div class="form-group">
+            <label for="message">Сообщение</label>
+            <textarea name="message" id="message" class="form-control" placeholder="33333">
+            </textarea>
+        </div>
+
+        <button type="submit" class="btn btn-success">Отправить</button>
     </form>
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
 
 Route::get('/about', function () {
     return view('about');
-});
+})->name('about');
 
 Route::get('/contact', function () {
     return view('contact');
-});
+})->name('contact');
+
+Route::post('/contact/submit/12421', function () {
+    return "Okay";
+})->name('contact-form');
